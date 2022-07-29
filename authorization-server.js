b50685a1d0da8503d22e0105d7b5ebc73f0566be
taiwotherobot <61283897+taiwotherobot@users.noreply.github.com>
2022-07-29 12:46:09 -0400

    h

--- authorization-server.js
+++ authorization-server.js
@@ -53,6 +53,73 @@ app.use(bodyParser.urlencoded({ extended: true }))
 /*
 Your code here
 */
+app.get("/authorize", (req, res) => {
+	const { clientId } = req.query
+	const client = clients[clientId]
+	if (!client) {
+		res.status(401).send("Invalid client")
+		return
+	} else {
+		res.status(200).end()
+	}
+	if (!containsAll(client.scopes, req.query.scope)) {
+		res.status(401).send("Invalid scope")
+		return
+	}
+	state = randomString()
+	res.render("login", { clientId, state, scopes: req.query.scope })
+
+})
+
+app.post("/approve", (req, res) => {
+	const { userName, password, requestId } = req.body
+	const request = requests[requestId]
+	if (!request) {
+		res.status(401).send("Invalid request")
+		return
+	}
+	if (userName !== request.userName || password !== users[request.userName]) {
+		res.status(401).send("Invalid credentials")
+		return
+	}
+	const code = randomString()
+	authorizationCodes[code] = {
+		clientReq: request.clientReq,
+		userName: request.userName,
+	}
+	res.redirect(`${request.redirectUri}?code=${code}`)
+})
+
+app.post("/token", (req, res) => {
+	const { grant_type, code, redirect_uri } = req.body
+	if (grant_type !== "authorization_code") {
+		res.status(400).send("Invalid grant_type")
+		return
+	}
+	const authCode = authorizationCodes[code]
+	if (!authCode) {
+		res.status(400).send("Invalid code")
+		return
+	}
+	if (redirect_uri !== authCode.clientReq.redirectUri) {
+		res.status(400).send("Invalid redirect_uri")
+		return
+	}
+	const accessToken = randomString()
+	const refreshToken = randomString()
+	const token = {
+		accessToken,
+		refreshToken,
+		expiresIn: 3600,
+		tokenType: "Bearer",
+		scope: authCode.clientReq.scope,
+	}
+	res.json(token)
+})
+
+
+
+
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
